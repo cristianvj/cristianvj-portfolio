be69f68d42081a1055f2aba45e0da41d322500c3
cristianvj <cristiandavid.vj@gmail.com>
2023-05-15 23:14:54 -0500

    added post detail

--- src/components/blog/PostDetail.tsx
+++ src/components/blog/PostDetail.tsx
@@ -25,13 +25,13 @@ const PostDetail: FC<PropsPostContent> = ({ post }) => (
               <Image
                 unoptimized
                 loader={grpahCMSImageLoader}
-                alt={post.author.name}
+                alt={post?.author?.name || ""}
                 height={30}
                 width={30}
                 className="align-middle rounded-full"
                 src={post.author.photo.url}
               />
-            <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+            <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post?.author?.name || ""}</p>
           </div>
           <div className="font-medium text-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
