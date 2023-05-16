a62cf8b1386160815c2847e6fcc2c894c70ef67d
cristianvj <cristiandavid.vj@gmail.com>
2023-05-15 22:36:58 -0500

    test with content post

--- src/components/blog/PostCard.tsx
+++ src/components/blog/PostCard.tsx
@@ -3,7 +3,10 @@ import React, { FC } from 'react'
 const PostCard: FC<any> = ({post, key}) => {
   console.log(post)
   return (
-    <h1>{post.title}</h1>
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.excerpt}</p>
+    </>
   )
 }
 
