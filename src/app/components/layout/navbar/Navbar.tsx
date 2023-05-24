00bffdf8fd037913313bb7362cc4e729c05e9d77
cristianvj <cristiandavid.vj@gmail.com>
2023-05-24 14:04:10 -0500

    added language button

--- src/app/components/layout/navbar/Navbar.tsx
+++ src/app/components/layout/navbar/Navbar.tsx
@@ -16,22 +16,22 @@ const pacifico = Pacifico({
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [showResponsiveMenu, setShowresponsiveMenu] = useState(false);
+  const [showLanguages, setShowLanguages] = useState(false);
 
   const path = usePathname();
-  console.log(path)
 
   return (
     <nav 
       className={`fixed z-10 w-full bg-white border-gray-200 ${darkMode === true && "dark:bg-gray-900"}`}
     >
-      <div className="flex flex-wrap md:flex-nowrap items-center justify-between md:container mx-auto py-4 px-10">
+      <div className="flex flex-wrap lg:flex-nowrap items-center justify-between lg:container mx-auto py-4 px-10">
         <Link href="/" className="flex items-center">
           <Image 
             src="/favicon.ico" 
-            className="h-8 mr-3" 
+            className="h-55 w-55 mr-3" 
             alt="Flowbite Logo" 
-            width={40} 
-            height={40}
+            width={50} 
+            height={50}
           />
           <span className={`
             self-center 
@@ -49,7 +49,23 @@ const Navbar = () => {
         <button 
           data-collapse-toggle="navbar-default" 
           type="button" 
-          className={`md:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 ${darkMode === true && "dark:hover:bg-gray-700 dark:focus:ring-gray-600 dark:text-gray-400"}`} 
+          className={`
+            lg:hidden 
+            inline-flex 
+            items-center 
+            p-1 
+            ml-3 
+            text-md 
+            text-gray-500 
+            rounded-lg  
+            hover:bg-gray-100 
+            focus:outline-none
+            ring-2 
+            ring-gray-500 
+            focus:ring-2 
+            focus:ring-gray-200 
+            ${darkMode === true && "dark:hover:bg-gray-700 dark:focus:ring-gray-600 dark:text-gray-400"}
+          `} 
           aria-controls="navbar-default" 
           aria-expanded="false"
           onClick={() => setShowresponsiveMenu(!showResponsiveMenu)}
@@ -74,10 +90,10 @@ const Navbar = () => {
           className={`
             ${showResponsiveMenu ? "show" : "hidden"} 
             w-full
-            md:w-auto
-            md:flex
-            md:flex-row
-            md:gap-4
+            lg:w-auto
+            lg:flex
+            lg:flex-row
+            lg:gap-4
           `} 
           id="navbar-default">
 
@@ -87,20 +103,20 @@ const Navbar = () => {
             flex 
             flex-col 
             p-4 
-            md:p-0 
+            lg:p-0 
             mt-4 
             border 
             border-gray-100 
             rounded-lg 
             bg-gray-50 
-            md:flex-row 
-            md:space-x-8 
-            md:mt-0 
-            md:border-0 
-            md:bg-white
-            ${darkMode === true && "dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900 "}`}
+            lg:flex-row 
+            lg:space-x-8 
+            lg:mt-0 
+            lg:border-0 
+            lg:bg-white
+            ${darkMode === true && "dark:border-gray-700 dark:bg-gray-800 lg:dark:bg-gray-900 "}`}
           >
-            <li className='md:m-auto'>
+            <li className='lg:m-auto'>
               <Link 
                 href="/" 
                 className={`
@@ -113,7 +129,7 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
-            <li className='md:m-auto'>
+            <li className='lg:m-auto'>
               <Link 
                 href="/blog" 
                 className={`
@@ -129,7 +145,7 @@ const Navbar = () => {
                 Blog
               </Link>
             </li>
-            <li className='md:m-auto'>
+            <li className='lg:m-auto'>
               <Link 
                 href="/interview" 
                 className={`
@@ -144,7 +160,7 @@ const Navbar = () => {
             </li>
           </ul>
 
-          <div className='mt-2 md:mt-0 md:ml-4 flex flex-nowrap justify-center'>
+          <div className='mt-2 lg:mt-0 lg:ml-4 flex flex-nowrap justify-center'>
             <a 
               target="_blank"
               href="https://github.com/cristianvj" 
@@ -152,7 +168,7 @@ const Navbar = () => {
               className={`${classes.navbarIcons.light} ${darkMode && classes.navbarIcons.dark}`}
               onClick={() => setShowresponsiveMenu(!showResponsiveMenu)}
             >
-              <svg className="w-5 h-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d={iconPaths.gitHub}></path></svg>
+              <svg className="w-6 h-6" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d={iconPaths.gitHub}></path></svg>
               <span className="sr-only">View on Github</span>
             </a>
 
@@ -177,6 +193,39 @@ const Navbar = () => {
               <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d={iconPaths.linkedin}></path></svg>
               <span className="sr-only">Flowbite LinkedIn</span>
             </a>
+
+            <div className="relative inline-block text-left">
+              <button 
+                type="button" 
+                data-dropdown-toggle="language-dropdown-menu" 
+                className={`${classes.navbarIcons.light} ${darkMode && classes.navbarIcons.dark} items-center hover:text-orange-600`}
+                onClick={() => setShowLanguages(!showLanguages)}
+              >
+                <svg aria-hidden="true" className="h-6 w-6 rounded-full mr-2" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us" viewBox="0 0 512 512"><g fill-rule="evenodd"><g stroke-width="1pt"><path fill="#bd3d44" d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/><path fill="#fff" d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/></g><path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)"/><path fill="#fff" d={iconPaths.us} transform="scale(3.9385)"/></g></svg> English
+              </button>
+                {/* !-- Dropdown --> */}
+
+                <div className={`z-50 ${!showLanguages && "hidden"} absolute my-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700`}>
+                  <ul className="py-2 font-medium" role="none">
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+                        <div className="inline-flex items-center">
+                          <svg aria-hidden="true" className="h-3.5 w-3.5 rounded-full mr-2" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us" viewBox="0 0 512 512"><g fill-rule="evenodd"><g stroke-width="1pt"><path fill="#bd3d44" d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/><path fill="#fff" d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/></g><path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)"/><path fill="#fff" d={iconPaths.us} transform="scale(3.9385)"/></g></svg>              
+                          English
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+                        <div className="inline-flex items-center">
+                          <svg className="h-3.5 w-3.5 rounded-full mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-it" viewBox="0 0 512 512"><g fill-rule="evenodd" stroke-width="1pt"><path fill="#fff" d="M0 0h512v512H0z"/><path fill="#009246" d="M0 0h170.7v512H0z"/><path fill="#ce2b37" d="M341.3 0H512v512H341.3z"/></g></svg>              
+                          Español
+                        </div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+            </div>
 
             {
               darkMode 
@@ -203,6 +252,10 @@ const Navbar = () => {
             }
 
           </div>
+
+          
+
+
 
         </div>
       </div>
