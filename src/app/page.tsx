b028629f08a10e4ef880b1d56c0e7a59f770cf7a
cristianvj <cristiandavid.vj@gmail.com>
2023-06-17 22:55:57 -0500

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import HeroCanvas from './../components/home/HeroCanvas';
 export default function Home() {
   return (
       <Main>
-        <section className={`relative w-full h-[calc(100vh_-_112px)] mx-auto flex`}>
-          <div className="w-full md:w-[50%]">
-            <h1 className='text-orange-600 text-2xl'>Hello, I&apos;m Cristian welcome to my portfolio</h1>
-            <Link className="text-white" href="/blog">Ir al blog</Link>
+        <section className={`relative w-full h-[calc(100vh_-_112px)] mx-auto flex flex-wrap`}>
+          <div className="w-full md:w-[45%] md:pt-4 md:pl-4">
+            <h1 className='text-orange-600 text-6xl'>Hello,</h1>
+            <h1 className='text-gray-200 text-7xl pt-2'>I&apos;m Cristian </h1>
+            <h2 className='text-gray-400 text-4xl pt-2'>welcome to my portfolio</h2>
           </div>
-          <div className="w-full md:w-[50%]">
+          <div className="w-full md:w-[45%]">
             <HeroCanvas />
           </div>
         </section>
