a0e9f8dd319a41b1980ff7697b62c51fc2e99a10
unknown <cristiandavid.vj@gmail.com>
2024-06-04 23:37:14 -0500

    fixed deploy error

---
+++ src/app/[locale]/interview/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Main } from '@/components';
+
+const InterviewQuestions = () => {
+  return (
+    <Main>
+      <div className='container mx-auto bg-red-400'>Front end road map</div>
+    </Main>
+  );
+};
+
+export default InterviewQuestions;
